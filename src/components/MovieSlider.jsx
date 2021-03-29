dd258d2f4919d71c2853686095c97e16342a44c9
bioback <78535998+bioback@users.noreply.github.com>
2021-03-29 13:21:21 +0200

    tweaks

--- src/components/MovieSlider.jsx
+++ src/components/MovieSlider.jsx
@@ -1,71 +1,63 @@
-import React from 'react'
-
-
+import React from "react";
+import { Spinner } from "react-bootstrap";
 class MovieSlider extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    isLoading: true,
+    isError: false,
+    movies: [],
+    numberResults: 7,
+  };
+  componentDidMount = async () => {
+    try {
+      let response = await fetch(
+        `http://www.omdbapi.com/?apikey=faa5255f&s=` + this.props.name
+      );
+      console.log(response);
+      let data = await response.json();
+      if (data.Response === "True") {
+        console.log(data);
+        this.setState({
+          movies: data.Search,
+          isLoading: false,
+          isError: false,
+        });
+      } else {
+        this.setState({
+          isLoading: false,
+          isError: true,
+        });
+      }
+    } catch (error) {
+      this.setState({
+        isLoading: false,
+        isError: true,
+      });
+      console.log(error);
     }
-    state = {
-        isLoading : false,
-        isError : false,
-        movies : [],
-        numberResults: 7
-    }
-    componentDidMount = async () => {
-        try {
-            let response = await fetch(`http://www.omdbapi.com/?apikey=faa5255f&s=` + this.props.name)
-            console.log(response)
-            if (response.ok) {
-                let data = await response.json()
-                console.log(data)
-                this.setState({
-                    movies: data.Search,
-                    isLoading: false
-                })
-            }   else {
-                    this.setState({
-                        isLoading: false,
-                        isError: true
-                    })
-            }
-        }   catch (error) {
-            this.setState({
-                isLoading: false,
-                isError: true
-            })
-            console.log(error)
-        }
-
-    }
-    render() {
-        return (
-            <>
-                <h5 className="ml-5 mt-2">{this.props.name}</h5>      
-                <div id="slider" className="container row ml-3 pt-0">
-                    {this.state.movies.slice(0, this.state.numberResults).map(mov => (
-                        <div className="item-container">
-                        <img
-                          className="item"
-                          src={mov.Poster}
-                        />
-                      </div>
-                    ))}
-                </div>
-            </>
-        )
-    }
-//     const [movieData, updateMovieData] => {useState([]);
-//   useEffect=(async () => {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     updateMovieData(data);
-//   }, []);
-// }}
-// console.log(movieData)
-
-
-
-
+  };
+  render() {
+    return this.state.isError ? (
+      <h1>ERROR</h1>
+    ) : this.state.isLoading ? (
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    ) : (
+      <>
+        <h5 className="ml-5 mt-2">{this.props.name}</h5>
+        <div id="slider" className="container row ml-3 pt-0">
+          {this.state.movies.slice(0, this.state.numberResults).map((mov) => (
+            <div className="item-container">
+              <img className="item" src={mov.Poster} />
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 
-export default MovieSlider
+export default MovieSlider;
